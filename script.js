8c4a4204cac0d8e4e84b9d1d50ce31ca5b1134d1
Liam <liammtech@gmail.com>
2025-05-25 15:17:27 +0100

    Clean up redundant functions

--- script.js
+++ script.js
@@ -21,11 +21,6 @@ resetButton.addEventListener("click", () => {
     renderPallette(gridSize)
 });
 
-function random_rgba() {
-    var o = Math.round, r = Math.random, s = 255;
-    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-}
-
 function renderPallette(size) {
     pallette.innerHTML = "";
     for (i = 0; i < (size * size); i++) {
